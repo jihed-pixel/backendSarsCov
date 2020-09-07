212488130a882ca2cb0beaacc9cecd0550156076
altaf <altafallahabbassi@gmail.com>
2020-09-08 00:12:28 +0100

    antecedents done

--- src/main/java/com/yobitrust/HachCovid19Back/Controllers/PatientController.java
+++ src/main/java/com/yobitrust/HachCovid19Back/Controllers/PatientController.java
@@ -1,14 +1,19 @@
 package com.yobitrust.HachCovid19Back.Controllers;
-import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import com.yobitrust.HachCovid19Back.Models.Patient;
+import com.yobitrust.HachCovid19Back.Models.PatientParts.AntecedentsMedicaux.*;
 import com.yobitrust.HachCovid19Back.Models.PatientParts.GeneralInformation;
+import com.yobitrust.HachCovid19Back.Models.PatientParts.HabitudesDeVie;
 import com.yobitrust.HachCovid19Back.Models.RequestModels.AddPatient;
+import com.yobitrust.HachCovid19Back.Models.RequestModels.AntecedentMedical;
+import com.yobitrust.HachCovid19Back.Models.RequestModels.RemoveAntecedent;
 import com.yobitrust.HachCovid19Back.Repositories.PatientRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -42,4 +47,139 @@ public class PatientController {
         return ResponseEntity.ok(patient);
 
     }
+    @PostMapping("/add-habitudes-de-vie/{cin}")
+    public ResponseEntity addHabitudesDeVie(@RequestBody HabitudesDeVie habitudesDeVie,@PathVariable Integer cin){
+        Patient patient=patientRepository.findByCin(cin);
+        if(patient==null)
+            return ResponseEntity.ok("Patient not found");
+
+        else patient.setHabitudesDeVie(habitudesDeVie);
+        patientRepository.save(patient);
+        return  ResponseEntity.ok(patient);
+    }
+    @PostMapping("/add-infos-generales/{cin}")
+    public ResponseEntity addInfosGenerales(@RequestBody GeneralInformation generalInformation ,@PathVariable Integer cin){
+        Patient patient =patientRepository.findByCin(cin);
+        if(patient==null)
+            return ResponseEntity.ok("Patient not found");
+        else patient.setGeneralInformation(generalInformation);
+        patientRepository.save(patient);
+        return ResponseEntity.ok(patient);
+    }
+    @PostMapping("/add-antecedents-medicaux/{cin}")
+    public ResponseEntity addAntecedentsMedicaux(@RequestBody AntecedentMedical antecedentMedical,@PathVariable Integer cin) {
+        Patient patient =patientRepository.findByCin(cin);
+        if(patient==null)
+            return ResponseEntity.ok("Patient not found");
+        ModelMapper mapper= new ModelMapper();
+        HashMap<String, AntecedentMedicaux> antecedents =patient.getAntecedentMedicaux();
+        if(antecedentMedical.getAntecedent().equals("grossesse")){
+
+            Grossesse grossesse = mapper.map(antecedentMedical, Grossesse.class);
+            antecedents.put("grossesse",grossesse);
+            patient.setAntecedentMedicaux(antecedents);
+
+        }
+        else if (antecedentMedical.getAntecedent().equals("PathResChronique")){
+            PathRespChronique pathRespChronique=mapper.map(antecedentMedical,PathRespChronique.class);
+            System.out.println(pathRespChronique.toString());
+            antecedents.put("PathResChronique",pathRespChronique);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("Cardiopathies")){
+            Cardiopathies cardiopathies=mapper.map(antecedentMedical,Cardiopathies.class);
+            antecedents.put("Cardiopathies",cardiopathies);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("TrRythCardiaque")){
+            TrRythCardiaque trRythCardiaque =mapper.map(antecedentMedical,TrRythCardiaque.class);
+            antecedents.put("TrRythCardiaque",trRythCardiaque);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("HTA")){
+            HTA hta =mapper.map(antecedentMedical,HTA.class);
+            antecedents.put("HTA",hta);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("Diabete")){
+            Diabete diabete=mapper.map(antecedentMedical,Diabete.class);
+            antecedents.put("Diabete",diabete);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("InsRenaleChro")){
+            InsRenaleChro insRenaleChro=mapper.map(antecedentMedical,InsRenaleChro.class);
+            antecedents.put("InsRenaleChro",insRenaleChro);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("AVC")){
+            AVC avc = mapper.map(antecedentMedical,AVC.class);
+            antecedents.put("AVC",avc);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("Retinopathie")){
+            Retinopathie retinopathie=mapper.map(antecedentMedical,Retinopathie.class);
+            antecedents.put("Retinopathie",retinopathie);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("ATCDchir")){
+            ATCDchir atcDchir =mapper.map(antecedentMedical,ATCDchir.class);
+            antecedents.put("ATCDchir",atcDchir);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("PriseAINS")){
+            PriseAINS priseAINS=mapper.map(antecedentMedical,PriseAINS.class);
+            antecedents.put("PriseAINS",priseAINS);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("Immunosuppreseur")){
+            Immunosuppreseur immunosuppreseur=mapper.map(antecedentMedical,Immunosuppreseur.class);
+            antecedents.put("Immunosuppreseur",immunosuppreseur);
+            patient.setAntecedentMedicaux(antecedents);
+        }
+        else if(antecedentMedical.getAntecedent().equals("AutresATCD")){
+            if(antecedents.containsKey("AutresATCD")){
+                List<String> autres= (List<String>) antecedents.get("AutresATCD");
+                autres.add(antecedentMedical.getAutres());
+                AutresATCD autresATCD = (AutresATCD) antecedents.get("AutresATCD");
+                autresATCD.setAutres(autres);
+                antecedents.put("AutresATCD", autresATCD);
+                patient.setAntecedentMedicaux(antecedents);
+
+            }
+            else {
+                AutresATCD autresATCD=new AutresATCD();
+                List<String> autres = autresATCD.getAutres();
+                autres.add(antecedentMedical.getAutres());
+                autresATCD.setAutres(autres);
+                antecedents.put("AutresATCD", autresATCD);
+                patient.setAntecedentMedicaux(antecedents);
+            }
+
+        }
+        patientRepository.save(patient);
+        return ResponseEntity.ok(patient);
+
+    }
+
+    @GetMapping("get-all-antecedents-medicaux/{cin}")
+    public  ResponseEntity getAllAntecedentsMedicaux(@PathVariable Integer cin){
+
+        Patient patient = patientRepository.findByCin(cin);
+        if(patient==null) return ResponseEntity.ok("No patient having \""+cin+"\"as cin ");
+        List<String> antecedents = new ArrayList<String>(patient.getAntecedentMedicaux().keySet());
+        return  ResponseEntity.ok(antecedents);
+
+    }
+    @PostMapping("remove-antecedent-medical/{cin}")
+    public ResponseEntity deleteAntecedentMedical(@PathVariable Integer cin , @RequestBody RemoveAntecedent antecedent){
+        Patient patient = patientRepository.findByCin(cin);
+        if(patient==null) return ResponseEntity.ok("No patient having \""+cin+"\"as cin ");
+        HashMap<String, AntecedentMedicaux> antecedents =patient.getAntecedentMedicaux();
+        antecedents.remove(antecedent.getAntecedent());
+        patient.setAntecedentMedicaux(antecedents);
+        patientRepository.save(patient);
+        return ResponseEntity.ok(patient.getAntecedentMedicaux().keySet());
+
+    }
 }
+
--- src/main/java/com/yobitrust/HachCovid19Back/Models/Patient.java
+++ src/main/java/com/yobitrust/HachCovid19Back/Models/Patient.java
@@ -1,9 +1,13 @@
 package com.yobitrust.HachCovid19Back.Models;
 
+import com.yobitrust.HachCovid19Back.Models.PatientParts.AntecedentsMedicaux.AntecedentMedicaux;
 import com.yobitrust.HachCovid19Back.Models.PatientParts.GeneralInformation;
+import com.yobitrust.HachCovid19Back.Models.PatientParts.HabitudesDeVie;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashMap;
 
 @Document
 public class Patient {
@@ -11,15 +15,28 @@ public class Patient {
     private Integer cin ;
     private Integer matricule ;
     private GeneralInformation generalInformation;
+    private HabitudesDeVie habitudesDeVie;
+    private HashMap<String, AntecedentMedicaux> antecedentMedicaux;
 
     public Patient() {
        // this.generalInformation=new GeneralInformation();
+        this.antecedentMedicaux=new HashMap<String, AntecedentMedicaux>();
     }
 
-    public Patient(Integer cin, Integer matricule,GeneralInformation generalInformation) {
+    public Patient(Integer cin, Integer matricule,GeneralInformation generalInformation,HabitudesDeVie habitudesDeVie,HashMap<String,AntecedentMedicaux>  antecedentMedicaux) {
         this.cin = cin;
         this.matricule = matricule;
         this.generalInformation=generalInformation;
+        this.habitudesDeVie=habitudesDeVie;
+        this.antecedentMedicaux=antecedentMedicaux;
+    }
+
+    public HashMap<String, AntecedentMedicaux> getAntecedentMedicaux() {
+        return antecedentMedicaux;
+    }
+
+    public void setAntecedentMedicaux(HashMap<String, AntecedentMedicaux> antecedentMedicaux) {
+        this.antecedentMedicaux = antecedentMedicaux;
     }
 
     public Integer getCin() {
@@ -44,5 +61,13 @@ public class Patient {
 
     public void setGeneralInformation(GeneralInformation generalInformation) {
         this.generalInformation = generalInformation;
+    }
+
+    public HabitudesDeVie getHabitudesDeVie() {
+        return habitudesDeVie;
+    }
+
+    public void setHabitudesDeVie(HabitudesDeVie habitudesDeVie) {
+        this.habitudesDeVie = habitudesDeVie;
     }
 }
